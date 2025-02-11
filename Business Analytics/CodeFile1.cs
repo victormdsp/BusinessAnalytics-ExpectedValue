fd1d7748b2f956465efbefe16b3862d3e836bf50
Victor Martini Domingues <victormdsp@hotmail.com>
2025-02-10 17:46:20 -0800

    Add project files.

---
+++ Business Analytics/CodeFile1.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+
+public class Program
+{
+    public static void Main()
+    {
+        string values = Console.ReadLine();
+        Console.WriteLine($@"Valores: {values}");
+
+    }
+}
