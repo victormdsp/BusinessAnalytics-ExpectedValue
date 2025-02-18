e45504a90d6dedcc9ef5cd441be84fb725eca6ae
Victor Martini Domingues <victormdsp@hotmail.com>
2025-02-17 20:58:51 -0800

    Expect Value same probability done

--- Business Analytics/CodeFile1.cs
+++ Business Analytics/CodeFile1.cs
@@ -7,18 +7,31 @@ public class Program
 {
     public static void Main()
     {
-        Program program = new Program();
-
-        Console.WriteLine("Press 1 to insert values with probability or 2 to only insert values (the probaility will be calculated equally): ");
-        string option = Console.ReadLine();
-
-        while (option != "1" || option != "2")
+        Console.WriteLine("Hi, this software calculate the expect value of given number.");
+        int runProgram = 1;
+        while(runProgram == 1)
         {
-            Console.WriteLine("Invalid option");
-            Console.WriteLine("Press 1 to insert values with probability or 2 to only insert values: ");
-        }
+            Program program = new Program();
 
-        decimal result = option == "1" ? program.expectedValueDiffProbability() : program.expectedValueEqualProbability();
+            Console.WriteLine("Press 1 to insert values with probability or 2 to only insert values (the probaility will be calculated equally): ");
+            int.TryParse(Console.ReadLine(), out int option);
+            Console.WriteLine("Option: {0}", option);
+
+            while (option != 1 && option != 2)
+            {
+                Console.WriteLine("Invalid option");
+                Console.WriteLine("Press 1 to insert values with probability or 2 to only insert values: ");
+                option = Console.Read();
+                Console.WriteLine("Option: {0}", option);
+
+            }
+
+            decimal result = option == 1 ? program.expectedValueDiffProbability() : program.expectedValueEqualProbability();
+            Console.WriteLine("Expected Value = {0}", result);
+
+            Console.WriteLine("If you want to calculate another expect value press 1, otherwise press any key to leave.");
+            runProgram = Console.Read();
+        }
     }
 
     public List<decimal> formatListToDecimal(string values)
@@ -37,13 +50,23 @@ public class Program
     {
         string values = this.showMessageGetValues("Insert the list of values separated by commas: ", "Example - value, value, ...");
         List<decimal> valuesFormated = this.formatListToDecimal(values);
-        return 1.2M;
+        decimal probabilityPerValue = 1M / valuesFormated.Count();
+        decimal result = 0M;
+
+        foreach(decimal value in valuesFormated)
+        {
+            result += value * probabilityPerValue;
+        }
+
+
+        return result;
     }
 
     public decimal expectedValueDiffProbability()
     {
         string values = this.showMessageGetValues("Insert the list of values and probability separated by semi-colons and commas: ", "Example - value;probability, value;probability, ...");
-        List<decimal> valuesFormatedDecimal = this.formatListToDecimal(values);
+        List<decimal> valuesFormated = this.formatListToDecimal(values);
+        Console.WriteLine($@"Given Value Decimal: {valuesFormated}");
         return 1.2M;
     }
 
